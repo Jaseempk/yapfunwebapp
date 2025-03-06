7952796a9378099680bd4ea99188754b7ba0cc4a
Jaseem <jasimpk011@gmail.com>
2025-03-07 00:56:35 +0530

    Initialize subgraph

---
+++ orderboofactory/src/yap-order-book-factory.ts
@@ -0,0 +1,74 @@
+import {
+  NewMarketInitialisedAndWhitelisted as NewMarketInitialisedAndWhitelistedEvent,
+  RoleAdminChanged as RoleAdminChangedEvent,
+  RoleGranted as RoleGrantedEvent,
+  RoleRevoked as RoleRevokedEvent
+} from "../generated/YapOrderBookFactory/YapOrderBookFactory"
+import {
+  NewMarketInitialisedAndWhitelisted,
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked
+} from "../generated/schema"
+
+export function handleNewMarketInitialisedAndWhitelisted(
+  event: NewMarketInitialisedAndWhitelistedEvent
+): void {
+  let entity = new NewMarketInitialisedAndWhitelisted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.kolId = event.params.kolId
+  entity.maker = event.params.maker
+  entity.marketAddy = event.params.marketAddy
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
+  let entity = new RoleAdminChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.previousAdminRole = event.params.previousAdminRole
+  entity.newAdminRole = event.params.newAdminRole
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleGranted(event: RoleGrantedEvent): void {
+  let entity = new RoleGranted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.account = event.params.account
+  entity.sender = event.params.sender
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleRevoked(event: RoleRevokedEvent): void {
+  let entity = new RoleRevoked(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.account = event.params.account
+  entity.sender = event.params.sender
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
