41703acbfe50c46d895407a94ff4a02121dad5ed
Jaseem <jasimpk011@gmail.com>
2025-03-10 00:23:55 +0530

    trigger_10-00:23:55

--- yapfunfe/build.sh
+++ yapfunfe/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+# Clear yarn cache
+yarn cache clean
+
 # Install dependencies using yarn
 yarn install --frozen-lockfile
+
+# Explicitly install TypeScript dependencies if needed
+yarn add --dev @types/node @types/react @types/react-dom typescript
 
 # Build the Next.js application
 yarn build 
