07e94c84c2c9f8272362b6a72c7e72c6ec249dda
Jaseem <jasimpk011@gmail.com>
2025-03-09 22:40:51 +0530

    feat yaml updated

--- yapfunfe/next.config.mjs
+++ yapfunfe/next.config.mjs
@@ -15,11 +15,34 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    domains: ['hub.kaito.ai'],
   },
   experimental: {
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
+  },
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/graphql',
+        destination: process.env.NEXT_PUBLIC_GRAPHQL_URL || 'https://yapfunbe.onrender.com/graphql',
+      },
+    ];
   },
 }
 
